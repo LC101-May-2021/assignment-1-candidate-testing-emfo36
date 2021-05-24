b32b3c861557751136f7228097926cc2390fa838
Emily Aasen <>
2021-05-24 19:50:57 +0000

    assignment 1 part 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,30 +3,38 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "Emily";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let question = "Who was the first American woman in space?";
+let correctAnswer = "Sally Ride";
+let candidateAnswer "Sally Ride";
+let questions =   ;
+let correctAnswers =  ;
+let candidateAnswers ;
 
 
-function askForName() {
+function askForName(candidateName)
+console.log(askforName + candidateName;) {
   // TODO 1.1b: Ask for candidate's name //
 
 }
 
-function askQuestion() {
+function askQuestion(Who was the first American woman in space?) {
+  if (candidateName === "Emily";
+    console.log(candidateName + question);
+let candidateResponse = "Sally Ride";
+let candidateResponse = candidateAnswer
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
 
 }
 
 function gradeQuiz(candidateAnswers) {
+  if (candidateAnswer === "Sally Ride" )
+    console.log(candidateAnswer + correctAnswer);
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //
+  
 
 
   let grade;
